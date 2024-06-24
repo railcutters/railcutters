3abbf9f6683dd813fbf508491d8e2a370507307d
Daniel Pereira <daniel@garajau.com.br>
2024-06-23 21:51:40 -0300

    fix: ensure we use the correct module names

--- lib/railcutters/logging/rails_ext.rb
+++ lib/railcutters/logging/rails_ext.rb
@@ -13,7 +13,7 @@ module Railcutters
       end
 
       def self.setup
-        return unless Rails.configuration.logger.is_a?(Railcutters::Logging::KVTaggedLogger)
+        return unless ::Rails.configuration.logger.is_a?(Railcutters::Logging::KVTaggedLogger)
 
         load_patches
         load_subscribers
@@ -62,10 +62,10 @@ module Railcutters
       # Injects and monkey patch Rails logging that is not covered by the log subscribers
       def self.load_patches
         require "rails/rack/logger"
-        Rails::Rack::Logger.prepend(RackLogger)
+        ::Rails::Rack::Logger.prepend(RackLogger)
 
         require "action_dispatch/middleware/debug_exceptions"
-        ActionDispatch::DebugExceptions.prepend(ActionDispatchDebugExceptions)
+        ::ActionDispatch::DebugExceptions.prepend(ActionDispatchDebugExceptions)
 
         require_relative "rails_ext/active_job_logging"
       end
@@ -75,13 +75,13 @@ module Railcutters
         ::ActiveSupport.on_load(:action_controller, run_once: true) do
           require "action_controller/log_subscriber"
           ::ActionController::LogSubscriber.detach_from(:action_controller)
-          LogSubscriber::ActionController.attach_to(:action_controller)
+          ::LogSubscriber::ActionController.attach_to(:action_controller)
         end
 
         ::ActiveSupport.on_load(:action_dispatch_request, run_once: true) do
           require "action_dispatch/log_subscriber"
           ::ActionDispatch::LogSubscriber.detach_from(:action_dispatch)
-          LogSubscriber::ActionDispatch.attach_to(:action_dispatch)
+          ::LogSubscriber::ActionDispatch.attach_to(:action_dispatch)
         end
 
         ::ActiveSupport.on_load(:action_view, run_once: true) do
@@ -92,13 +92,13 @@ module Railcutters
         ::ActiveSupport.on_load(:active_storage_record, run_once: true) do
           require "active_storage/log_subscriber"
           ::ActiveStorage::LogSubscriber.detach_from(:active_storage)
-          LogSubscriber::ActiveStorage.attach_to(:active_storage)
+          ::LogSubscriber::ActiveStorage.attach_to(:active_storage)
         end
 
         ::ActiveSupport.on_load(:active_job, run_once: true) do
           require "active_job/log_subscriber"
           ::ActiveJob::LogSubscriber.detach_from(:active_job)
-          LogSubscriber::ActiveJob.attach_to(:active_job)
+          ::LogSubscriber::ActiveJob.attach_to(:active_job)
         end
       end
     end
