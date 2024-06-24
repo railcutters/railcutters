7118c82cb834ef4b844c345e30b4aa5e7e4f8ab1
Daniel Pereira <daniel@garajau.com.br>
2024-06-23 21:52:28 -0300

    chore: rename config keys

--- lib/railcutters/railtie.rb
+++ lib/railcutters/railtie.rb
@@ -19,22 +19,22 @@ require "active_record/connection_adapters"
 module Railcutters
   class Railtie < ::Rails::Railtie
     initializer "railcutters.load_action_controller" do
-      next unless config.railcutters.use_params_renamer
+      next unless config.railcutters.params_renamer
 
       ::ActionController::Parameters.include(ActionController::ParamsRenamer)
     end
 
     initializer "railcutters.load_sqlite_configs" do
-      if config.railcutters.use_sqlite_tuning
+      if config.railcutters.sqlite_tuning
         ::ActiveSupport.on_load(:active_record_sqlite3adapter) do
           # self refers to `SQLite3Adapter` here, so we can call .prepend directly
           prepend(ActiveRecord::ConnectionAdapters::SQLite3Tuning)
         end
       end
 
-      if config.railcutters.use_sqlite_strictness
+      if config.railcutters.sqlite_strictness
         # Configures SQLite with a strict strings mode, which disables double-quoted string literals.
-        config.sqlite3_adapter_strict_strings_by_default = true
+        config.active_record.sqlite3_adapter_strict_strings_by_default = true
 
         ::ActiveSupport.on_load(:active_record_sqlite3adapter) do
           # self refers to `SQLite3Adapter` here, so we can call .prepend directly
@@ -50,7 +50,7 @@ module Railcutters
     end
 
     initializer "railcutters.load_migration_addons" do
-      next unless config.railcutters.active_record_migration_defaults
+      next unless config.railcutters.ar_migration_defaults
 
       ::ActiveRecord::ConnectionAdapters::TableDefinition.prepend(
         ActiveRecord::ConnectionAdapters::DefaultTimestamps
@@ -59,13 +59,13 @@ module Railcutters
     end
 
     initializer "railcutters.load_active_record" do
-      next if config.railcutters.active_record_enum_defaults.blank?
+      next if config.railcutters.ar_enum_defaults.blank? && !config.railcutters.ar_enum_string_values
 
       ::ActiveRecord::Base.extend(ActiveRecord::EnumDefaults)
     end
 
-    initializer "railcutters.load_normalize_payload_keys" do
-      next unless config.railcutters.normalize_payload_keys
+    initializer "railcutters.load_normalized_payload" do
+      next unless config.railcutters.normalized_payload
 
       if defined?(Jbuilder)
         ::Jbuilder.key_format(camelize: :lower)
@@ -78,20 +78,20 @@ module Railcutters
     end
 
     initializer "railcutters.load_pagination" do
-      next unless config.railcutters.use_pagination
+      next unless config.railcutters.pagination
 
       ::ActiveRecord::Base.include(ActiveRecord::Pagination)
       ::ActionController::Metal.include(ActionController::Pagination)
     end
 
     initializer "railcutters.load_safe_sort" do
-      next unless config.railcutters.use_safe_sort
+      next unless config.railcutters.safe_sort
 
       ::ActiveRecord::Base.include(ActiveRecord::SafeSort)
     end
 
     initializer "railcutters.load_logging", after: :initialize_logger do
-      next unless config.railcutters.use_hashed_tagged_logging
+      next unless config.railcutters.hashed_tagged_logging
 
       Logging::RailsExt.setup
     end
@@ -103,37 +103,38 @@ module Railcutters
 
     # Enable loading the params renamer, and allows parameters renaming from within controllers
     # using an easy syntax
-    config.railcutters.use_params_renamer = true
+    config.railcutters.params_renamer = true
 
     # Enable a simple pagination helper for controllers and models, that exposes a `paginate` method
     # to the controller and the model, and sets the pagination metadata on the response using the
     # Pagination header.
-    config.railcutters.use_pagination = true
+    config.railcutters.pagination = true
 
     # Enable a new method (safe_sort) on models so that you can expose it safely to users and it's
     # guaranteed to be validated against a list of allowed columns.
-    config.railcutters.use_safe_sort = true
+    config.railcutters.safe_sort = true
 
     # Use SQLite3 strict mode
+    #
     # WARNING: this will affect new tables created with `rails db:migrate`, and you will not be able
     # to disable it once you enable it.
-    config.railcutters.use_sqlite_strictness = true
+    config.railcutters.sqlite_strictness = true
 
     # Use sensible SQLite3 defaults to get more performance out of the box
-    config.railcutters.use_sqlite_tuning = true
+    config.railcutters.sqlite_tuning = true
 
     # Use better defaults when creating and running migrations.
     # It will set the default value of every created_at/updated_at to the CURRENT_TIMESTAMP function
     # in the database, ensuring that it is created with the right values even outside rails.
     # It will also set `null: false` on the migrations. It is already the default behavior, but this
     # will make it explicit on the table definition.
-    config.railcutters.active_record_migration_defaults = true
+    config.railcutters.ar_migration_defaults = true
 
     # Use better defaults for ActiveRecord::Enum. Pass nil or an empty hash to use Rails' defaults.
     #
     # WARNING: this will affect existing code that uses `enum`, so you should only enable this if
     # you're starting a new project or are willing to change your existing code.
-    config.railcutters.active_record_enum_defaults = {
+    config.railcutters.ar_enum_defaults = {
       # Keeping a prefix for all methods generated by a enum is a good idea to avoid conflicts
       prefix: true,
 
@@ -155,7 +156,7 @@ module Railcutters
     #
     # WARNING: this will affect existing code that uses `enum`, so you should only enable this if
     # you're starting a new project or are willing to change your existing code.
-    config.railcutters.active_record_enum_use_string_values = true
+    config.railcutters.ar_enum_string_values = true
 
     # This will normalize the keys of the payload sent to and from the controller to be snake_case
     # instead of camelCase.
@@ -166,7 +167,7 @@ module Railcutters
     #
     # WARNING: this will affect existing code that rely on using camelCase keys, so you should only
     # enable this if you're starting a new project or are willing to change your existing code.
-    config.railcutters.normalize_payload_keys = true
+    config.railcutters.normalized_payload = true
 
     # This will force Rails internal logging to use a KVTaggedLogger instead of the default string
     # interface. This will not have any effect if you use Rails' standard Logger. In order to enable
@@ -176,15 +177,16 @@ module Railcutters
     #   $stdout,
     #   formatter: Railcutters::Logging::HumanFriendlyFormatter.new
     # )
-    config.railcutters.use_hashed_tagged_logging = true
+    config.railcutters.hashed_tagged_logging = true
 
     # This is a helper method to set all the defaults to a safe value, meaning that it will not make
     # any changes to the default behavior of Rails. This is useful if you are installing this gem
     # in an existing project and don't want to change any default behavior.
     config.railcutters.define_singleton_method(:use_safe_defaults!) do
-      railcutters.active_record_enum_defaults = nil
-      railcutters.active_record_enum_use_string_values = false
-      railcutters.normalize_payload_keys = false
+      railcutters.ar_enum_defaults = nil
+      railcutters.ar_enum_string_values = false
+      railcutters.sqlite_strictness = false
+      railcutters.normalized_payload = false
     end
   end
 end
